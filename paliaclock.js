df4467c70090d62b2d290b6ee85526c631dd0d75
Brad Hines <brad.hines@outlook.com>
2023-10-18 16:30:52 -0400

    Started making graphics and a moving clock hand

--- paliaclock.js
+++ paliaclock.js
@@ -8,6 +8,7 @@ function getCurrentTime() {
 
     //Figure out Palia Time
     var paliaSeconds = (currentSeconds * 24) + (currentMinutes * 60 * 24);
+    var paliaDayProgress = paliaSeconds / 86400;
     var paliaHour = (paliaSeconds / 60) / 60;
     var paliaAMPMValue = "";
 
@@ -15,6 +16,9 @@ function getCurrentTime() {
     if (currentPaliaHour > 12) {
         currentPaliaHour = currentPaliaHour - 12
         paliaAMPMValue = "PM";
+    } else if (currentPaliaHour == 0) {
+        currentPaliaHour = 12;
+        paliaAMPMValue = "AM";
     } else {
         paliaAMPMValue = "AM";
     }
@@ -27,29 +31,10 @@ function getCurrentTime() {
         currentPaliaMinute = "00";
     }
 
+    var clockHand = document.getElementsByClassName("PaliaClock_hand")[0];
+    var clockHandRotation = `${(paliaDayProgress * 360)}deg`;
     paliaTimeDiv.innerText = `${currentPaliaHour}:${currentPaliaMinute} ${paliaAMPMValue}`;
-
-
-    // var minutesToDay = currentMinutes / 60;
-    // var secondsToMinutes = (currentSeconds / 60) / 100;
-    // var paliaTimeScale = minutesToDay + secondsToMinutes;
-    // var paliaDayScale = paliaTimeScale * 24;
-    // var paliaDayScaleDecimalIndex = paliaDayScale.toString().indexOf(".");
-    // var paliaHour = Math.floor(paliaDayScale);
-    // var paliaMinutesRaw = paliaDayScale.toString().substring(paliaDayScaleDecimalIndex + 1);
-    // var paliaMinutes = 0;
-
-    // if (paliaMinutesRaw.length == 1) {
-    //     paliaMinutes = `${paliaMinutesRaw}0`
-    // } else if (paliaMinutesRaw.length == 2) {
-    //     paliaMinutes = paliaMinutesRaw
-    // } else if (paliaMinutesRaw.length > 2) {
-    //     paliaMinutes = paliaMinutesRaw.slice (0,2)
-    // }
-    
-    // paliaTimeDiv.innerHTML = `<p>Time in Palia: ${paliaHour}:${paliaMinutes}</p>
-    // <p>Day scale value: ${paliaDayScale}</p>
-    // <p>Time Scale value: ${paliaTimeScale}</p>`;
+    clockHand.style.rotate = clockHandRotation;
 
     //Wait 1 sec, run this function again;
     setTimeout(function () {
